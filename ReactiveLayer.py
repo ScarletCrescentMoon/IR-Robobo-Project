b0d95e9c2a77c0b1a662297c3bf17f9f5a9f5643
unknown <kriszta.soos04@e-uvt.ro>
2026-01-15 12:25:07 +0100

    New function HowFar + RunLayer modified

--- ReactiveLayer.py
+++ ReactiveLayer.py
@@ -1,16 +1,9 @@
 from robobopy.Robobo import Robobo
 from robobopy.utils.Color import Color
 from robobopy.utils.IR import IR
+from robobopy.utils.Wheels import Wheels
 import Global
 import time
-
-'''
-# TODO:
-    HowFar(self) - returns the distance traveled in cm and the orientation
-    
-    self.distanceLastTraveled - approximation of how far the robot has traveled since last running HowFar()
-    
-'''
 
 class ReactiveLayer:
     def __init__(self, roboID, ip):
@@ -18,6 +11,9 @@ class ReactiveLayer:
         self.ID = roboID
         self.robot = Robobo(ip)
         self.robot.connect()
+
+        self.robot.resetWheelEncoders()
+        self.distanceLastTraveled = 0
         self.robotIsDone = False
         time.sleep(1)
 
@@ -116,13 +112,19 @@ class ReactiveLayer:
             return 0
         
         try:
-            f_left = sensors['Front-L']
-            f_center = sensors['Front-C']
-            f_right = sensors['Front-R']
+            if isinstance(sensors, dict):
+
+                f_left = sensors['Front-L']
+                f_center = sensors['Front-C']
+                f_right = sensors['Front-R']
+            else:
+                f_left = sensors[2]
+                f_center = sensors[3]
+                f_right = sensors[4]
             
             max_front = max(f_left, f_center, f_right)
 
-            if max_front > 500:
+            if max_front > 320000:
                 print(f"Obstacle detected! Vaalue: {max_front}")
                 return max_front
                 
@@ -133,7 +135,7 @@ class ReactiveLayer:
 
     def escape_wall(self):
         
-        self.robot.moveWheels(-40, -40)
+        self.robot.moveWheels(-25, -25)
         time.sleep(1.5)
         self.stop_robot()
 
@@ -155,17 +157,31 @@ class ReactiveLayer:
                 self.rotate_to_direction(self.current_orientation, self.goal_orientation)
                 self.current_orientation = self.goal_orientation
 
+                self.HowFar()
+
+                self.move_forward()
+                time.sleep(0.5)
+
             obstacle_dist = self.check_and_report_obstacle()
-            if obstacle_dist > 3000:
+            if obstacle_dist > 320000:
+                print(f"STOP! Limit: {obstacle_dist}")
                 self.stop_robot()
-                self.escape_wall()
+                
+                self.robot.moveWheels(-30, -30)
+                time.sleep(1.0)
+                self.stop_robot()
+
                 current_val = int(self.current_orientation.value)
                 new_val = (current_val + 2) % 8 
                 self.set_goal_orientation(Global.Orientation(new_val))
-                self.set_goal_orientation(Global.Orientation(new_val))
                 print(f"Orientation changed")
+    
+            elif obstacle_dist > 250000:
+                self.robot.moveWheels(5,5)
+
             else:
                 self.move_forward()
+            
             time.sleep(0.1)
         
         print(f"Robot {self.ID} finished task")
@@ -177,3 +193,28 @@ class ReactiveLayer:
         #      rotate_to_direction(current, goal)
         #   else
         #      self.move_forward()
+
+    def HowFar(self):
+        '''
+        # TODO:
+            HowFar(self) - returns the distance traveled in cm and the orientation
+            
+            self.distanceLastTraveled - approximation of how far the robot has traveled since last running HowFar()
+            
+        '''
+        # readWheelEncoders() returns a list : [left_encoder, right_encoder]
+
+        pos_left = self.robot.readWheelPosition(Wheels.L)
+        pos_right = self.robot.readWheelPosition(Wheels.R)
+
+        if pos_left is None: pos_left = 0
+        if pos_right is None: pos_right = 0
+
+        self.robot.resetWheelEncoders()
+
+        CONVERSION_FACTOR = 0.056
+        avg_degrees = (pos_left + pos_right) / 2.0
+        self.distanceLastTraveled = avg_degrees * CONVERSION_FACTOR
+
+        return (self.distanceLastTraveled, self.current_orientation)
+       
--- test_robobo.py
+++ test_robobo.py
@@ -4,12 +4,18 @@ import time
 
 def debug_reactive_layer():
     # 1. Initialization and ID Test
-    layer = ReactiveLayer(roboID=1, ip="127.0.0.1")
+    layer = ReactiveLayer(roboID=1, ip="192.168.1.87")
     
     # Force detection to False to test movement and obstacle logic indefinitely
     layer.detect_target_robot = lambda: False
 
     print(f"DEBUG: Testing get_robot_id: {layer.get_robot_id()}")
+
+    # Test HowFar
+
+    print("\n Test: HowFar at starting")
+    dist, ori = layer.HowFar()
+    print(f"DEBUG: Distance should be close to 0: {dist:.2f} cm, Orientation: {ori}")
 
     # 2. Orientation Tests (get/set)
     print(f"DEBUG: Initial orientation: {layer.get_current_orientation()}")
@@ -24,11 +30,25 @@ def debug_reactive_layer():
         print(f"DEBUG: IR Sensor Check {i+1}: {val}")
         time.sleep(1)
 
+    # Test HowFar while mooving
+    print("\n Test: HowFar while moving")
+    print("Robot moves forward for 2 seconds")
+    layer.move_forward()
+    time.sleep(2)
+    layer.stop_robot()
+
+    dist, ori = layer.HowFar()
+    print(f"DEBUG: Distance result: {dist:.2f} cm to orientation {ori}")
+
     # 4. Manual Rotation Test
     print("DEBUG: Testing rotation to goal")
     layer.rotate_to_direction(layer.current_orientation, layer.goal_orientation)
     layer.current_orientation = layer.goal_orientation
     print(f"DEBUG: Orientation after rotation: {layer.get_current_orientation()}")
+
+    # Test HowFar - at rotation distance should be 0
+    dist_rot, ori_rot = layer.HowFar()
+    print(f"DEBUG: After rotation, distance {dist_rot:.2f} cm, new orientation: {ori_rot}")
 
     # 5. RunLayer Test (Movement + Escape Logic)
     print("DEBUG: Starting RunLayer. Robot will move and avoid walls.")
@@ -36,6 +56,9 @@ def debug_reactive_layer():
         layer.RunLayer()
     except KeyboardInterrupt:
         layer.stop_robot()
+        # Test HowFar
+        final_dist, _ = layer.HowFar()
+        print(f"\nDEBUG: Final distance: {final_dist:.2f} cm")
         print("DEBUG: Test stopped by user")
 
 if __name__ == "__main__":
